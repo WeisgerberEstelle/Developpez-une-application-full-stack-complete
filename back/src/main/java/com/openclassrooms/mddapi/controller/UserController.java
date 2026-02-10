0bcef9bcc2e88262a08c262dadb4fb5e7afb8522
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2026-02-10 17:30:09 +0100

    feat(controller): add update profile root

--- back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.openclassrooms.mddapi.controller;
 
+import com.openclassrooms.mddapi.dto.UpdateUserRequest;
 import com.openclassrooms.mddapi.dto.UserResponse;
 import com.openclassrooms.mddapi.entity.User;
 import com.openclassrooms.mddapi.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -20,5 +20,12 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<UserResponse> me(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(userService.getCurrentUser(user));
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<UserResponse> updateMe(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestBody UpdateUserRequest request) {
+        return ResponseEntity.ok(userService.updateUser(user, request));
     }
 }
