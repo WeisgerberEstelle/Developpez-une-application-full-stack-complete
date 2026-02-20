e7426d3217a92e83647e12fa6f1dd6a8237cfcf1
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2026-02-20 19:49:19 +0100

    refactor: remove standalone components from AppModule declarations

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
-import { TopicsListComponent } from './features/topics/topics-list/topics-list.component';
-import { ProfileComponent } from './features/profile/profile.component';
-import { FeedComponent } from './features/feed/feed.component';
-import { PostDetailComponent } from './features/posts/post-detail/post-detail.component';
-import { CreatePostComponent } from './features/posts/create-post/create-post.component';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { HeaderComponent } from './shared/header/header.component';
 
@@ -20,20 +13,13 @@ import { HeaderComponent } from './shared/header/header.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    TopicsListComponent,
-    ProfileComponent,
-    FeedComponent,
-    PostDetailComponent,
-    CreatePostComponent,
     HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
+    NgOptimizedImage,
   ],
   providers: [provideHttpClient(withInterceptors([jwtInterceptor]))],
   bootstrap: [AppComponent],
